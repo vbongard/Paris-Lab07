4b866e3ee96b8dc080bbefa58d202bc4d0fcd778
Jonathan Roman <jroman8@u.rochester.edu>
2018-10-10 11:32:29 -0400

    Finished styles
    If you have any comments or problems with the current style let me know and I'll try to make changes before class!

--- index.php
+++ index.php
@@ -2,17 +2,17 @@
 
 <body>
 
-<header class="topleft">
+<header>
 	<h1>Student Bios</h1>
 </header>
 <?php include "inc/nav.inc";?> 
-<main>
-<p class="belowPic">This website has been created for the CSC 174 class at the University of Rochester. Many students enjoy Computer Science class at the University of Rochester and the University is known for its rigorous and prestigious computer science program. Many students participate in the Computer Science major, which is a part of the Hajim School of Engineering. The Hajim School of Engineering offers undergraduate and graduate degrees in fields related to Computer Science. Though many students participate in engineering majors, not all students do. Some students participate in other majors such as Business, Film and Media Studies, Political Science, Biology, Studio Art, and more.</p> <!-- feel free to add more content here if you need to! I'm not sure how long this needs to be in order to best fill the space.-->
-</main>
 <figure class="centerImage">
 	<img src="images/comp_sci.jpeg" alt="Computer Science">
 </figure>
-<section>
+<main>
+<p class="belowPic">This website has been created for the CSC 174 class at the University of Rochester. Many students enjoy Computer Science class at the University of Rochester and the University is known for its rigorous and prestigious computer science program. Many students participate in the Computer Science major, which is a part of the Hajim School of Engineering. The Hajim School of Engineering offers undergraduate and graduate degrees in fields related to Computer Science. Though many students participate in engineering majors, not all students do. Some students participate in other majors such as Business, Film and Media Studies, Political Science, Biology, Studio Art, and more.</p> <!-- feel free to add more content here if you need to! I'm not sure how long this needs to be in order to best fill the space.-->
+</main>
+<section class="listBelow">
 <h3>Students and their Majors</h3>
 <ul>
 	<li>Jayda Medina - Computer Science Major</li>
